160d78ca4ece40a5b9ee04b8b24298761aaa6ff7
Simeon Carstens <simeon.carstens@tweag.io>
2020-05-20 12:31:32 +0200

    Finish documentation of code

--- simulation.py
+++ simulation.py
@@ -4,8 +4,25 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from celluloid import Camera
 
+
 class Person(object):
-    def __init__(self, pos, vel, in_prob, out_prob, death_prob, infected=False, immune=False):
+    def __init__(self, pos, vel, in_prob, out_prob, death_prob, infected=False,
+                 immune=False):
+        """
+        Make a new person object.
+
+        Args:
+            pos (np.ndarray): The x/y position vector of the person.
+            vel (np.ndarray): The x/y velocity vector of the person.
+            in_prob (float): Probability to get infected in an
+                             encounter with an infected person.
+            out_prob (float): Probability to infect another person in
+                              an encounter.
+            death_prob (float): Probability of dying in a single time
+                                step if person is infected.
+            infected (bool): Whether the person is infected or not
+            immune (bool): Whether person is immune or not
+        """
         self.pos = pos
         self.vel = vel
         self.in_prob = in_prob
@@ -21,6 +38,10 @@ class Person(object):
         return self._dead
     @dead.setter
     def dead(self, value):
+        """
+        Sets a person's status to dead. This involves setting
+        their infection status to False and their velocity to zero.
+        """
         if self.dead and value == False:
             raise("Zombie mode not implemented yet")
         elif ~self.dead and value == True:
@@ -36,14 +57,37 @@ class Room(object):
 
 
 class Simulation(object):
-    def __init__(self, room, persons, prob_dist, dt, cutoff, transmit_cutoff, k, time_to_heal):
+    def __init__(self, room, persons, prob_dist, dt, cutoff, transmit_cutoff,
+                 force_constant, time_to_heal):
+        """
+        Arguments:
+        
+        - room (room): A room object
+        - persons (list): A list of persons
+        - prob_dist (callable): A function describing a base probability of
+                                infection depending on the distance between
+                                two persons
+        - dt (float): Time step for numerical integration of equations of 
+                      motion
+        - cutoff (float): Cutoff distance between two persons below which
+                          the force pushing them away from each other kicks
+                          in
+        - transmit_cutoff (float): Cutoff distance between two persons above
+                                   which no transmission can happen
+        - force_constant (float): Force constant determining strength of 
+                                  interaction potential between two persons.
+                                  The higher it is, the more persons bounce
+                                  off each other.
+        - time_to_heal (int): Number of time steps it takes for a person to
+                              become healthy again after they've been infected
+        """
         self.room = room
         self.persons = persons
         self.prob_dist = prob_dist
         self.dt = dt
         self.cutoff = cutoff
         self.transmit_cutoff = transmit_cutoff
-        self.k = k
+        self.force_constant = force_constant
         poss = np.array([p.pos for p in self.persons])
         self._oldgrad = self.gradient(poss)
         self._current_step = 0
@@ -51,6 +95,15 @@ class Simulation(object):
         
     
     def _maybe_transmit(self, p1, p2, d):
+        """
+        Transmits disease between two persons depending on their distance
+        
+        Arguments:
+
+        - p1 (Person): first person
+        - p2 (Person): second person
+        - d (float): distance between the two persons
+        """
         if (p1.infected & (~p2.infected)) or ((~p1.infected) & p2.infected):
             base_prob = self.prob_dist(d)
             if p1.infected & ~p2.immune:
@@ -62,10 +115,21 @@ class Simulation(object):
 
 
     def gradient(self, pos):
+        """
+        Gradient of potential energy responsable for making persons
+        bounce off each other. Includes call to _maybe_transmit
+        in order to avoid a second double loop.
+
+        Arguments:
+        
+        - pos (np.ndarray): 2D numpy array of position vectors of
+                            all persons
+        """
         dm = np.linalg.norm(pos[None, :] - pos[:, None], axis=2)
         res = np.zeros(pos.shape)
         for i, pos1 in enumerate(pos):
             if self.persons[i].dead:
+                ## dead persons don't interact with others
                 continue
             for j, pos2 in enumerate(pos[i+1:], i+1):
                 if self.persons[j].dead:
@@ -76,10 +140,27 @@ class Simulation(object):
                     res[i] += pos1 - pos2
                     res[j] -= pos1 - pos2
                     
-        return self.k * res
+        return self.force_constant * res
                 
 
     def _bounce_off_walls(self, new_poss, old_poss, new_vels, old_vels, new_grad):
+        """
+        Corrects positions and velocities of persons who exceed the room limits
+        
+        Arguments:
+
+        - new_poss (np.ndarray): 2D numpy array of updated position vectors
+        - old_poss (np.ndarray): 2D numpy array of old position vectors
+        - new_vels (np.ndarray): 2D numpy array of updated velocity vectors
+        - old_vels (np.ndarray): 2D numpy array of old velocity vectors
+        - new_grad (np.ndarray): updated gradient vector (equivalent to 
+                                 acceleration of persons)
+
+        Returns:
+
+        - (new_poss, new_vels, new_grad): corrected positions, velocities,
+                                          and gradient
+        """
         ## left and right wall
         # person exceeds left wall, but is within upper and lower walls
         l_coll = (new_poss[:,0] < 0) & (new_poss[:,1] > 0) \
@@ -121,9 +202,18 @@ class Simulation(object):
 
 
     def _integration_step(self, poss, vels):
-        '''
+        """
         One single velocity Verlet integration step
-        '''
+
+        Arguments:
+
+        - poss (np.ndarray): 2D numpy array of position vectors
+        - vels (np.ndarray): 2D numpy array of velocity vectors
+
+        Returns:
+
+        - (poss, vels, new_grad): Updated positions, velocities, and gradient
+        """
         poss = poss.copy()
         vels = vels.copy()
         poss += vels * self.dt + 0.5 * self._oldgrad * self.dt * self.dt
@@ -134,6 +224,9 @@ class Simulation(object):
     
     
     def _move_persons(self):
+        """
+        Update movement of all persons during one time step
+        """
         old_poss = np.array([p.pos for p in self.persons])
         old_vels = np.array([p.vel for p in self.persons])
         new_poss, new_vels, new_grad = self._integration_step(old_poss,
@@ -153,6 +246,9 @@ class Simulation(object):
         
 
     def step(self):
+        """
+        Advance the simulation by one time step.
+        """
         self._move_persons()
 
         for p in self.persons:
@@ -232,13 +328,16 @@ def setup_axes(fig, room, n_steps, n_persons):
 
     return main_ax, inf_ax, immune_ax, fatalities_ax
 
-    
+
+## Initialize simulation
 n_persons = 500
 room = Room(80, 80)
+# persons start with random positions and velocities
 persons = [Person(np.random.uniform(low=(0,0), high=(room.w, room.h)),
                   np.random.uniform(low=(-2, -2), high=(2, 2), size=2),
                   1., 1., 0.01, False)
            for _ in range(n_persons)]
+# some persons actually start out being infected
 chosen_ones = np.random.choice(np.arange(n_persons), n_persons // 50)
 for i in chosen_ones:
     persons[i].infected = True
@@ -246,11 +345,11 @@ sim = Simulation(room, persons, lambda d:  d < 1,
                  dt=0.1,
                  cutoff=1,
                  transmit_cutoff=2,
-                 k=20,
+                 force_constant=20,
                  time_to_heal=40)
 
-
-n_steps = 70
+## Run simulation
+n_steps = 300
 all_positions = [np.array([p.pos for p in sim.persons])]
 all_infected = [np.array([True if p.infected else False
                           for i, p in enumerate(sim.persons)])]
@@ -258,7 +357,12 @@ all_immune = [np.array([p.immune for p in sim.persons])]
 all_fatalities = [np.array([p.dead for p in sim.persons])]
 
 for i in range(n_steps):
+    if i % 50 == 0:
+        print(f"Simulating step {i}/{n_steps}...")
     sim.step()
+
+    ## keep track of all positions, infection status, immunity
+    ## status and dead persons
     poss = np.array([p.pos for p in sim.persons])
     all_positions.append(poss)
     infected = np.array([p.infected for p in sim.persons])
@@ -269,12 +373,14 @@ for i in range(n_steps):
     all_fatalities.append(fatalities)
 
 
-## animation starts here
+## Animate everything
 fig = plt.figure()
 camera = Camera(fig)
 main_ax, inf_ax, immune_ax, fatalities_ax = setup_axes(fig, room,
                                                        n_steps, n_persons)
 for step, poss in enumerate(all_positions):
+    if step % 50 == 0:
+        print(f"Animating step {step}/{n_steps}...")
     plot_infected(step, poss, all_infected, sim, main_ax, inf_ax)
     plot_notinfected(step, poss, all_infected, sim, main_ax)
     plot_dead(step, poss, all_fatalities, sim, main_ax, fatalities_ax)
@@ -284,4 +390,4 @@ for step, poss in enumerate(all_positions):
     camera.snap()
 
 anim = camera.animate(blit=True)
-anim.save("bla.mp4", fps=10)
+anim.save("output.mp4", fps=10)
